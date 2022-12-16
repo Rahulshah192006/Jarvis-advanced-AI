be5a63bc14b280b9852d1807be1fdd6b003a8397
Rahulshah192006 <rahul.shah102006@gmail.com>
2022-12-16 23:22:21 +0530

    JARVIS

--- JARVIS.py
+++ JARVIS.py
@@ -1,1 +1,20 @@
-print("hello")
+import speech_recognition as sr
+from googletrans import Translator
+
+def Listen():
+    mic = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening.....")
+        mic.pause_threshold = 1
+        audio = mic.listen(source,0,2)
+
+    try:
+        print("Thinking .....")
+        query = mic.recognize_google(audio,language="en-in")
+        print(f"You said : {query}\n")
+    except:
+        return ""
+
+    query = str(query).lower()
+    return query
+Listen()
